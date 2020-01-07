2e7daaa68d79c6467e91bbd9d5ebfcf34729f6a5
Alex Carney <alcarneyme@gmail.com>
2020-01-07 19:43:04 +0000

    Simplifications

--- src/vkdevice.c
+++ src/vkdevice.c
@@ -2,43 +2,6 @@
 #include <stdlib.h>
 #include <vulkan/vulkan.h>
 
-void getVkPhysicalDevices(VkInstance vk, VkPhysicalDevice** physical_devices, uint32_t* count)
-{
-    // First check to see how many Vulkan compatiable devices are available
-    VkResult res = vkEnumeratePhysicalDevices(vk, count, NULL);
-    if (res != VK_SUCCESS) {
-        fprintf(stderr, "Unable to enumerate physical devices\n");
-        return;
-    }
-
-    // printf("Found %d devices\n", *count);
-    *physical_devices = (VkPhysicalDevice*) malloc(*count * sizeof(VkPhysicalDevice));
-    if (*physical_devices == NULL) {
-        fprintf(stderr, "Unable to enumerate physical devices\n");
-        return;
-    }
-
-    res = vkEnumeratePhysicalDevices(vk, count, *physical_devices);
-    if (res != VK_SUCCESS) {
-        fprintf(stderr, "Unable to enumerate physical devices\n");
-        free(physical_devices);
-        physical_devices = NULL;
-    }
-}
-
-void getVkPhysicalDeviceProperties(VkPhysicalDeviceProperties** device_properties, VkPhysicalDevice* physical_devices, uint32_t count)
-{
-    *device_properties = (VkPhysicalDeviceProperties*) malloc(count * sizeof(VkPhysicalDeviceProperties));
-    if (*device_properties == NULL) {
-        fprintf(stderr, "Unable to get physical device properties\n");
-        return;
-    }
-
-    for (uint32_t i = 0; i < count; i++) {
-        vkGetPhysicalDeviceProperties(physical_devices[i], device_properties[i]);
-    }
-}
-
 const char* vkPhysicalDeviceType_as_string(VkPhysicalDeviceType type) {
     switch (type)
     {
@@ -67,9 +30,7 @@ int main()
     VkApplicationInfo app_info = {
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
         .pApplicationName = "vkDevice Info",
-        .applicationVersion = 0x010000,
-        .pEngineName = "vkEngine",
-        .engineVersion = 0x010000,
+        .applicationVersion = VK_MAKE_VERSION(1,0,0),
         .apiVersion = VK_API_VERSION_1_1,
     };
 
@@ -88,46 +49,52 @@ int main()
 
     /* Check to see how many Vulkan compatible devices there are. */
     uint32_t count = 0;
-    VkPhysicalDevice* physical_devices = NULL;
-    getVkPhysicalDevices(vk, &physical_devices, &count);
+    res = vkEnumeratePhysicalDevices(vk, &count, NULL);
+    if (res != VK_SUCCESS) {
+        fprintf(stderr, "Unable to enumerate physical devices\n");
+        goto cleanup_instance;
+    }
 
+    VkPhysicalDevice* physical_devices = malloc(count * sizeof(VkPhysicalDevice));
     if (physical_devices == NULL) {
-        goto cleanup;
+        fprintf(stderr, "Unable to enumerate physical devices\n");
+        goto cleanup_instance;
+    }
+
+    res = vkEnumeratePhysicalDevices(vk, &count, physical_devices);
+    if (res != VK_SUCCESS) {
+        fprintf(stderr, "Unable to enumerate physical devices\n");
+        goto cleanup_devices;
     }
 
     // Now, a VkPhysicalDevice doesn't actually tell us anything but is just a way to
     // reference a particular device. To find out its details we need to make additional
     // calls...
-    VkPhysicalDeviceProperties* device_properties = NULL;
-    getVkPhysicalDeviceProperties(&device_properties, physical_devices, count);
-
-    if (device_properties == NULL) {
-        free(physical_devices);
-        goto cleanup;
-    }
-
     for (uint32_t i = 0; i < count; i++) {
+        VkPhysicalDeviceProperties properties = {};
+        vkGetPhysicalDeviceProperties(physical_devices[i], &properties);
 
-        uint32_t vk_major = VK_VERSION_MAJOR(device_properties[i].apiVersion);
-        uint32_t vk_minor = VK_VERSION_MINOR(device_properties[i].apiVersion);
-        uint32_t vk_patch = VK_VERSION_PATCH(device_properties[i].apiVersion);
+        uint32_t vk_major = VK_VERSION_MAJOR(properties.apiVersion);
+        uint32_t vk_minor = VK_VERSION_MINOR(properties.apiVersion);
+        uint32_t vk_patch = VK_VERSION_PATCH(properties.apiVersion);
 
-        uint32_t driver_major = VK_VERSION_MAJOR(device_properties[i].driverVersion);
-        uint32_t driver_minor = VK_VERSION_MINOR(device_properties[i].driverVersion);
-        uint32_t driver_patch = VK_VERSION_PATCH(device_properties[i].driverVersion);
+        uint32_t driver_major = VK_VERSION_MAJOR(properties.driverVersion);
+        uint32_t driver_minor = VK_VERSION_MINOR(properties.driverVersion);
+        uint32_t driver_patch = VK_VERSION_PATCH(properties.driverVersion);
 
-        printf("Device Name:     \t%s\n", device_properties[i].deviceName);
-        printf("  Type:          \t%s\n", vkPhysicalDeviceType_as_string(device_properties[i].deviceType));
-        printf("  Vendor ID:     \t%d\n", device_properties[i].vendorID);
-        printf("  Device ID:     \t%d\n", device_properties[i].deviceID);
+        printf("Device Name:     \t%s\n", properties.deviceName);
+        printf("  Type:          \t%s\n", vkPhysicalDeviceType_as_string(properties.deviceType));
+        printf("  Vendor ID:     \t%d\n", properties.vendorID);
+        printf("  Device ID:     \t%d\n", properties.deviceID);
         printf("  API Version:   \tv%d.%d.%d\n", vk_major, vk_minor, vk_patch);
         printf("  Driver Version:\tv%d.%d.%d\n", driver_major, driver_minor, driver_patch);
     }
 
-    free(physical_devices);
-    free(device_properties);
+    cleanup_devices:
+        free(physical_devices);
+        physical_devices = NULL;
 
-    cleanup:
+    cleanup_instance:
         vkDestroyInstance(vk, NULL);
 
     return 0;
